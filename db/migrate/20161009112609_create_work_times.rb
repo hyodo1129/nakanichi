e4ed5f30b7b4760d928e4a97585e0fb201803eae
wolkins <h.fukuhara0606@gmail.com>
2016-10-09 20:42:08 +0900

    bundle exec rails g model work_time

---
+++ db/migrate/20161009112609_create_work_times.rb
@@ -0,0 +1,11 @@
+class CreateWorkTimes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :work_times do |t|
+      t.belongs_to :user
+      t.integer  :operate_time, default: 0, null: false
+      t.datetime :operate_date, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826015837) do
+ActiveRecord::Schema.define(version: 20161009112609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 20160826015837) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "work_times", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "operate_time", default: 0, null: false
+    t.datetime "operate_date",             null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["user_id"], name: "index_work_times_on_user_id", using: :btree
   end
 
 end
